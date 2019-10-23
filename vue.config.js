cdcff0fac3c844977aeef9b0625db4db9fb8b47d
László Nits <nitslaszlo@gmail.com>
2019-10-23 03:07:52 +0200

    up

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    devServer: {
-        host: '192.168.1.68',
-        port: 8080
-    },
+    // devServer: {
+    //     host: '192.168.1.68',
+    //     port: 8080
+    // },
     configureWebpack: (config) => {
         config.devtool = 'source-map'
     }
