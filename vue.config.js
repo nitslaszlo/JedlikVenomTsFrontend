7b231d57cddef3930550d3907129a9f23fb55414
László Nits <nitslaszlo@gmail.com>
2019-11-13 02:22:32 +0100

    up

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+  // Hiba nélküli távoli teszthez kell csak, de ilyenkor nincs http://localhost:8080 a fejlesztő gépen
+  // A frontend fordítását újra kell indítani!
+  // Az ip számot írd át a frontend Network címére és portjára, pl.:
   // devServer: {
-  //     host: "192.168.1.68",
-  //     port: 8080
+  //   host: "192.168.1.68",
+  //   port: 8080
   // },
   configureWebpack: config => {
     config.devtool = "source-map";
