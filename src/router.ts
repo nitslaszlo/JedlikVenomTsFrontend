3c95bc81444a8de996a464fbd3bcad8690e50a85
László Nits <nitslaszlo@gmail.com>
2019-11-13 02:15:28 +0100

    up

--- src/router.ts
+++ src/router.ts
@@ -1,41 +1,37 @@
 import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
+import Router from "vue-router";
 import CsudijoView from "./views/CsudijoView.vue";
 import HomeView from "./views/HomeView.vue";
 import VuexDemoView from "./views/VuexDemoView.vue";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes: RouteConfig[] = [
-  {
-    component: HomeView,
-    name: "home",
-    path: "/"
-  },
-  {
-    component: VuexDemoView,
-    name: "vuexDemo",
-    path: "/vuexdemo"
-  },
-  {
-    component: CsudijoView,
-    name: "csudijo",
-    path: "/csudijo"
-  },
-  {
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import("./views/AboutView.vue"),
-    name: "about",
-    path: "/about"
-  }
-];
-
-const router: VueRouter = new VueRouter({
-  mode: "history",
+export default new Router({
   base: process.env.BASE_URL,
-  routes
+  mode: "history",
+  routes: [
+    {
+      component: HomeView,
+      name: "home",
+      path: "/"
+    },
+    {
+      component: VuexDemoView,
+      name: "vuexDemo",
+      path: "/vuexdemo"
+    },
+    {
+      component: CsudijoView,
+      name: "csudijo",
+      path: "/csudijo"
+    },
+    {
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("./views/AboutView.vue"),
+      name: "about",
+      path: "/about"
+    }
+  ]
 });
-
-export default router;
