1d724f4830904e058f82f333933d732c573c7f2e
László Nits <nitslaszlo@gmail.com>
2019-11-13 02:19:30 +0100

    up

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -2,8 +2,6 @@ import Vue, { VNode } from "vue";
 
 declare global {
   namespace JSX {
-    // tslint:disable no-empty-interface
-    // tslint:disable interface-name
     interface Element extends VNode {}
     interface ElementClass extends Vue {}
     interface IntrinsicElements {
