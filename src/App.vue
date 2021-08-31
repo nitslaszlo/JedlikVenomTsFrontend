54fb4e6cd3aa67fbe1f2b7ab084fb642c10f3ef5
nitslaszlo <nitslaszlo@gmail.com>
2021-08-31 19:44:04 +0200

    up

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item :to="{ name: 'csudijo' }">Csudijo</b-nav-item>
-          <b-nav-item :to="{ name: 'vuexDemo' }">VuexDemo</b-nav-item>
-          <b-nav-item :to="{ name: 'home' }">Home</b-nav-item>
-          <b-nav-item :to="{ name: 'about' }">About</b-nav-item>
+          <b-nav-item :to="{ path: '/csudijo' }">Csudijo</b-nav-item>
+          <b-nav-item :to="{ path: '/vuexDemo' }">VuexDemo</b-nav-item>
+          <b-nav-item :to="{ path: '/home' }">Home</b-nav-item>
+          <b-nav-item :to="{ path: '/about' }">About</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
